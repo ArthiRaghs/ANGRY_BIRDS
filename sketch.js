4fccaf96b2adc5698b554ae732efe88f4fec0f4c
ArthiRaghs <anugopiarthi@gmail.com>
2020-12-12 15:47:30 +0530

    Code added for Platform

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var engine,world;
 var box1,box2,ground;
 var pig1,log1;
 var pig3,log3,box3,box4;
-var bird;
+var bird,platform;
 
 
 function setup() {
@@ -13,6 +13,7 @@ function setup() {
   engine=Engine.create();
   world=engine.world;
   ground=new Ground(600,height,1200,20);
+  platform=new Ground(150,305,300,170);
   box1=new Box(700,320,70,70);
   box2=new Box(920,320,70,70);
   pig1=new Pig(810,350);
@@ -52,7 +53,7 @@ function draw() {
   log4.display();
   log5.display();
   bird.display();
-
+  platform.display();
   
   
 }
