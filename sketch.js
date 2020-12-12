45410b501c13a2db28492882d49e12babe51cb58
ArthiRaghs <anugopiarthi@gmail.com>
2020-12-12 16:27:13 +0530

    comment added for platform

--- sketch.js
+++ sketch.js
@@ -13,6 +13,7 @@ function setup() {
   engine=Engine.create();
   world=engine.world;
   ground=new Ground(600,height,1200,20);
+  // for slingshot on 12 Dec
   platform=new Ground(150,305,300,170);
   box1=new Box(700,320,70,70);
   box2=new Box(920,320,70,70);
